e80332c4c35c7f86b3f137dd56cd16f815bcd983
renzon <renzon@gmail.com>
2017-06-13 20:16:35 -0300

    Adiconado valores ao grafico de barrasπ

--- descritores/benchmark.py
+++ descritores/benchmark.py
@@ -22,9 +22,13 @@ def create_instances_in_one_second(cls):
 
 versoes_item_pedido = (s.ItemPedido for s in (s1, s2, s3, s4, s5, s6, s7))
 tempos = list(map(create_instances_in_one_second, versoes_item_pedido))
+_, ax = pyplot.subplots()
 pyplot.title('Criação de Objs ItemPedido')
 pyplot.ylabel('Número de objetos criados em 1 segundo')
 pyplot.xlabel('Versões de ItemPedido')
-pyplot.bar(list(range(1, 8)), tempos)
-pyplot.grid(True)
+x_values = list(range(1, 8))
+pyplot.bar(x_values, tempos)
+for x, tempo in zip(x_values, tempos):
+    ax.text(x - 0.4, tempo + 12000, str(tempo), color='black', fontweight='bold')
+pyplot.grid(True, axis='y')
 pyplot.show()
